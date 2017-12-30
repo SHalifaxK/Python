8e1a451d49f82230b3acbf93221c6a5b939ad586
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2017-12-30 22:18:45 +0200

    Create for_each.py

---
+++ for_each.py
@@ -0,0 +1,8 @@
+#for this one lets use a list and go through it item by item
+
+#first lets define a list
+list = [1,2,3,4]
+
+#then lets print every item in the list
+for item in list:
+    print item
