d9cac915b0ee1260210c2d06023d2234fc69b5df
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2017-12-30 22:38:19 +0200

    Update if_else.py

--- if_else.py
+++ if_else.py
@@ -1,7 +1,8 @@
 #lets ask a positive number from the user and if it is less than 10 lets print less than 10 and otherwise greater than 10
 num = input("Please give me a positive number")
-if num<10
-    print "less than 10"
-else
-    print "greater than 10"
-    
+
+#if-else
+if int(num)<10:
+    print ("less than 10")
+else:
+    print ("greater than 10")
