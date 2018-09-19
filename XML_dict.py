579bb180b83b2a7a86cadb90cd1cbf87701f1772
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-09-19 10:57:35 +0300

    Create XML_dict.py

---
+++ XML_dict.py
@@ -0,0 +1,18 @@
+'''
+xmltodics usage
+'''
+
+import xmltodict
+
+'''
+Use context manager to read in the xml and conver it to dict
+'''
+
+with open('L1_param.xml','r') as f:
+    tree = xmltodict.parse(f.read())
+
+'''
+print out the 'nested value' that you want from dict
+'''
+
+print(tree['L1_parameters']['Parameters']['ant_port_layer'])
